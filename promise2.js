2ca5e732a4957d431b1e60dc719b1824e121b76e
Abhimanyu Aryan <abhimanyu.chat@gmail.com>
2018-08-11 12:31:34 +0530

    handling rejection in promise

--- promise2.js
+++ promise2.js
@@ -4,10 +4,15 @@ function setup() {
     delay(1000)
     .then(() => createP('hello'))
     .catch((err) => console.error(err))
+
+    delay('promising');
 }
 
 function delay(time){
     return new Promise((resolve, reject) => {
+        if(isNaN(time)) {
+            reject();
+        }
         setTimeout(resolve, time);
     })
 
