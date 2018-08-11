0b8b4c15de6c9a86a2855a96003a7af4cc70b8f1
Abhimanyu Aryan <abhimanyu.chat@gmail.com>
2018-08-11 13:21:17 +0530

    refactoring then to async await

--- promise2.js
+++ promise2.js
@@ -8,6 +8,26 @@ function setup() {
     delay('promising');
 }
 
+async function delayES8(time) {
+    // If below function returns a promise
+    await delay(time); // kind of like writing blocking code
+    // await someThingElse();
+    // let val = await SomethingElseElse();
+
+    return;
+}
+
+
+
+
+
+
+
+
+
+
+
+
 function delay(time){
     return new Promise((resolve, reject) => {
         if(isNaN(time)) {
